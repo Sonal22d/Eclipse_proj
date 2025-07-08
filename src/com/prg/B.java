9efbf0bf823152a5bbca5dbbfc345d6baf4bc935
Sonal22d <sdd1526@gmail.com>
2025-07-08 16:26:43 +0530

    3rd_commit

--- src/com/prg/B.java
+++ src/com/prg/B.java
@@ -4,6 +4,7 @@ public class B extends A {
 	public void m2() {
 		System.out.println("Hello...Good afternoon");
 		System.out.println("hi..all");
+		System.out.println("hi..");
 	}
 
 	public static void main(String[] args) {
